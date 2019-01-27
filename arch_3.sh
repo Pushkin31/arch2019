16bff04d13cf86b35abc6190818e3bede1b1a647
Pushkin31 <35974828+Pushkin31@users.noreply.github.com>
2019-01-28 02:16:14 +0300

    Update arch_3.sh

--- arch_3.sh
+++ arch_3.sh
@@ -18,20 +18,7 @@ cd ..
 rm -rf aurman
 
 echo 'Установка программ'
-sudo pacman -S firefox ufw obs-studio veracrypt filezilla gimp libreoffice libreoffice-fresh-ru  screenfetch vlc qbittorrent f2fs-tools dosfstools ntfs-3g alsa-lib alsa-utils gnome-calculator file-roller p7zip unrar gvfs aspell-ru pulseaudio gwenview --noconfirm
-
-sudo aurman -S  joxi obs-linuxbrowser xflux xflux-gui-git hunspell-ru pamac-aur --noconfirm 
-
-echo 'Установка тем'
-aurman -S osx-arc-shadow breeze-obsidian-cursor-theme papirus-maia-icon-theme-git --noconfirm
-
-echo 'Создаем нужные директории'
-sudo pacman -S xdg-user-dirs
-xdg-user-dirs-update
-
-echo 'Ставим лого ArchLinux в меню'
-wget ordanax.ru/arch/archlinux_logo.png
-sudo mv -f ~/Загрузки/archlinux_logo.png /usr/share/pixmaps/archlinux_logo.png
+sudo pacman -S firefox  gimp libreoffice libreoffice-fresh-ru  screenfetch vlc qbittorrent f2fs-tools dosfstools ntfs-3g alsa-lib alsa-utils gnome-calculator file-roller p7zip pulseaudio gwenview --noconfirm
 
 
 echo 'Включаем сетевой экран'
