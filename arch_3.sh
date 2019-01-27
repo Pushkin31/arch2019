651ded412782e295b382643c01a230efc7fbfc3b
Pushkin31 <35974828+Pushkin31@users.noreply.github.com>
2019-01-27 21:27:29 +0300

    Create arch_3.sh

---
+++ arch_3.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+echo 'Установка AUR (aurman)'
+sudo pacman -Syy
+sudo pacman -S git --noconfirm
+
+#Ставим зависимость expac-git
+git clone https://aur.archlinux.org/expac-git.git
+cd expac-git
+makepkg -si --noconfirm
+cd ..
+rm -rf expac-git
+
+#Ставим aurman
+git clone https://aur.archlinux.org/aurman.git
+cd aurman
+makepkg -si --noconfirm --skippgpcheck
+cd ..
+rm -rf aurman
+
+echo 'Установка программ'
+sudo pacman -S firefox ufw obs-studio veracrypt filezilla gimp libreoffice libreoffice-fresh-ru  screenfetch vlc qbittorrent f2fs-tools dosfstools ntfs-3g alsa-lib alsa-utils gnome-calculator file-roller p7zip unrar gvfs aspell-ru pulseaudio gwenview --noconfirm
+
+sudo aurman -S  joxi obs-linuxbrowser xflux xflux-gui-git hunspell-ru pamac-aur --noconfirm 
+
+echo 'Установка тем'
+aurman -S osx-arc-shadow breeze-obsidian-cursor-theme papirus-maia-icon-theme-git --noconfirm
+
+echo 'Создаем нужные директории'
+sudo pacman -S xdg-user-dirs
+xdg-user-dirs-update
+
+echo 'Качаем и устанавливаем настройки Xfce'
+# Чтобы сделать копию ваших настоек XFCE выполните команду ниже.
+# tar -czf xfce4.tar.gz ~/.config/xfce4
+# Выгрузите архив в интернет и скорректируйте ссылку на XFCE файл заменив ссылку http://ordanax.ru/arch_linux/xfce4.tar.gz на свою.
+#cd ~/Downloads
+#wget http://ordanax.ru/arch/xfce4.tar.gz
+#rm -rf ~/.config/xfce4
+#tar -xzf xfce4.tar.gz -C /
+
+echo 'Ставим лого ArchLinux в меню'
+wget ordanax.ru/arch/archlinux_logo.png
+sudo mv -f ~/Загрузки/archlinux_logo.png /usr/share/pixmaps/archlinux_logo.png
+
+
+echo 'Включаем сетевой экран'
+sudo ufw enable
+
+echo 'Установка завершена!'
+rm -rf ~/arch_3.sh
