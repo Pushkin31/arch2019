e823fea4e33099fcf91ffc1a5dab081dd173af13
Pushkin31 <35974828+Pushkin31@users.noreply.github.com>
2019-02-08 00:49:06 +0300

    Update arch_3.sh

--- arch_3.sh
+++ arch_3.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
-echo 'Установка AUR (aurman)'
-sudo pacman -Syy
-sudo pacman -S git --noconfirm
+sudo pacman -Syu
 
-#Ставим зависимость expac-git
-git clone https://aur.archlinux.org/expac-git.git
-cd expac-git
-makepkg -si --noconfirm
-cd ..
-rm -rf expac-git
+mkdir -p /tmp/yay_install
+cd /tmp/yay_install
 
-#Ставим aurman
-git clone https://aur.archlinux.org/aurman.git
-cd aurman
-makepkg -si --noconfirm --skippgpcheck
-cd ..
-rm -rf aurman
+echo 'Установка "yay" из AUR'
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -sir --needed --noconfirm --skippgpcheck
+rm -rf yay_install
+
+
+sudo pacman -Syu
+
+mkdir -p /tmp/pamac-aur_install
+cd /tmp/pamac-aur_install
+
+'Установка "pamac-aur" из AUR'
+git clone https://aur.archlinux.org/pamac-aur.git
+cd pamac-aur
+makepkg -si --needed --noconfirm --skippgpcheck
+rm -rf pamac-aur_install
+
 
 echo 'Установка программ'
 sudo pacman -S firefox  gimp libreoffice libreoffice-fresh-ru  screenfetch vlc qbittorrent f2fs-tools dosfstools ntfs-3g alsa-lib alsa-utils gnome-calculator file-roller p7zip pulseaudio gwenview --noconfirm
