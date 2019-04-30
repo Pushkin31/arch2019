f2e57cdeb3db3fe211f3a60534c800952c730afc
Pushkin31 <pushkinaleks10@gmail.com>
2019-04-30 18:28:38 +0300

    no message

--- arch1.sh
+++ arch1.sh
@@ -21,52 +21,20 @@ echo 'Ссылка на чек лист есть в группе vk.com/arch4u'
 echo '2.3 Синхронизация системных часов'
 timedatectl set-ntp true
 
-echo '2.4 создание разделов'
-(
-  echo n;
-  echo;
-  echo;
-  echo;
-  echo +200M;
-
-  echo n;
-  echo;
-  echo;
-  echo;
-  echo +25G;
-
-  echo n;
-  echo;
-  echo;
-  echo;
-  echo +25G;
-  echo a;
-  echo 1;
-  
-  #echo n;
-  #echo;
-  #echo;
-  #echo;
-  #echo;
-  #echo a;
-  #echo 1;
-
-  echo w;
-) | fdisk /dev/sda
 
 echo 'Ваша разметка диска'
 fdisk -l
 
 echo '2.4.2 Форматирование дисков'
-mkfs.ext2  /dev/sda1 -L boot
-mkfs.ext4  /dev/sda2 -L root
-mkfs.ext4  /dev/sda3 -L home
+mkfs.ext2  /dev/sda3 -L boot
+mkfs.ext4  /dev/sda4 -L root
+mkfs.ext4  /dev/sda5 -L home
 
 echo '2.4.3 Монтирование дисков'
-mount /dev/sda2 /mnt
+mount /dev/sda4 /mnt
 mkdir /mnt/{boot,home}
-mount /dev/sda1 /mnt/boot
-mount /dev/sda3 /mnt/home
+mount /dev/sda3 /mnt/boot
+mount /dev/sda5 /mnt/home
 
 echo '3.1 Выбор зеркал для загрузки. Ставим зеркало от Яндекс'
 echo "Server = http://mirror.yandex.ru/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
