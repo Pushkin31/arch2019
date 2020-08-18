354e5b9c0ad0c1a55f656f26e72b651b5c938ee1
Alexander <35974828+Pushkin31@users.noreply.github.com>
2020-08-18 17:54:03 +0300

    Update arch1.sh

--- arch1.sh
+++ arch1.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
-# Установочный скрипт ALFI (от Arch Linux Full Install - Полная установка Arch Linux)
-# Цель скрипта - быстрое развертывание системы с вашими персональными настройками (KDE(или XFCE), темы, программы и т.д.).
-
-# В разработке принимали участие:
-# Алексей Бойко https://vk.com/ordanax
-# Степан Скрябин https://vk.com/zurg3
-# Михаил Сарвилин https://vk.com/michael170707
-# Данил Антошкин https://vk.com/danil.antoshkin
-
-# Корректировали:
-# Александр Хрипушин https://vk.com/aleks_hripushin
-# Артём Духовный https://vk.com/hoasker
-
-echo 'Скрипт сделан на основе чеклиста Бойко Алексея по Установке ArchLinux'
-echo 'Ссылка на чек лист есть в группе vk.com/arch4u'
+# Arch Linux Fast Install - Быстрая установка Arch Linux https://github.com/ordanax/arch
+# Цель скрипта - быстрое развертывание системы с вашими персональными настройками (конфиг XFCE, темы, программы и т.д.).
+# Автор скрипта Алексей Бойко https://vk.com/ordanax
 
 loadkeys ru
 setfont cyr-sun16
@@ -22,11 +10,17 @@ setfont cyr-sun16
 echo '2.3 Синхронизация системных часов'
 timedatectl set-ntp true
 
-  echo n;
-  echo;
-  echo;
-  echo;
-  echo +200M;
+echo '2.4 создание разделов'
+{
+ echo g;
+
+ echo n;
+ echo ;
+ echo;
+ echo +300M;
+ echo y;
+ echo t;
+ echo 1;
 
   echo n;
   echo;
@@ -42,14 +36,6 @@ timedatectl set-ntp true
   echo a;
   echo 1;
 
-  #echo n;
-  #echo;
-  #echo;
-  #echo;
-  #echo;
-  #echo a;
-  #echo 1;
-
   echo w;
 ) | fdisk /dev/sda
 
@@ -57,21 +43,24 @@ echo 'Ваша разметка диска'
 fdisk -l
 
 echo '2.4.2 Форматирование дисков'
-mkfs.ext2  /dev/sda3 -L boot
-mkfs.ext4  /dev/sda4 -L root
-mkfs.ext4  /dev/sda5 -L home
+mkfs.fat -F32 /dev/sda1 -L boot
+mkfs.ext4  /dev/sda3 -L root
+mkfs.ext4  /dev/sda4 -L home
 
 echo '2.4.3 Монтирование дисков'
-mount /dev/sda4 /mnt
+mount /dev/sda3 /mnt
 mkdir /mnt/{boot,home}
-mount /dev/sda3 /mnt/boot
-mount /dev/sda5 /mnt/home
+mount /dev/sda1 /mnt/boot
+mount /dev/sda4 /mnt/home
 
 echo '3.1 Выбор зеркал для загрузки. Ставим зеркало от Яндекс'
 echo "Server = http://mirror.yandex.ru/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
+rm -rf /etc/pacman.d/mirrorlist
+wget https://git.io/mirrorlist
+mv -f ~/mirrorlist /etc/pacman.d/mirrorlist
 
 echo '3.2 Установка основных пакетов'
-pacstrap /mnt base base-devel
+pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd netctl
 
 echo '3.3 Настройка системы'
 genfstab -pU /mnt >> /mnt/etc/fstab
